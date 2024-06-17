5ba7421fdea51f87a063283b4e148d9da027f8ab
huyinf <136902882+huyinf@users.noreply.github.com>
2024-06-17 21:41:42 +0700

    scroll cart items

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -29,26 +29,30 @@ const CartItems = () => {
                 <p>Remove</p>
             </div>
             <hr />
-            {allProduct.map((e) => {
-                if (cartItems[e.id] > 0) {
-                    console.log(e.id);
 
-                    return (
-                        <div key={e.id}>
-                            <div className="cart-items-format cart-items-format-main">
-                                <img src={e.thumbnail_url} alt="" className='cart-icon-product-icon' />
-                                <p>{e.name}</p>
-                                <p>{formatPrice(e.new_price)}</p>
-                                <button className='cart-items-quantity'>{cartItems[e.id]}</button>
-                                <p>{formatPrice(e.new_price * cartItems[e.id])}</p>
-                                <img src={remove_icon} onClick={() => { removeFromCart(e.id) }} alt="" className="cart-icon-remove-icon" />
+            <div className="cart-items-scroll">
+                {allProduct.map((e) => {
+                    if (cartItems[e.id] > 0) {
+                        console.log(e.id);
+
+                        return (
+                            <div key={e.id}>
+                                <div className="cart-items-format cart-items-format-main">
+                                    <img src={e.thumbnail_url} alt="" className='cart-icon-product-icon' />
+                                    <p>{e.name}</p>
+                                    <p>{formatPrice(e.new_price)}</p>
+                                    <button className='cart-items-quantity'>{cartItems[e.id]}</button>
+                                    <p>{formatPrice(e.new_price * cartItems[e.id])}</p>
+                                    <img src={remove_icon} onClick={() => { removeFromCart(e.id) }} alt="" className="cart-icon-remove-icon" />
+                                </div>
+                                <hr />
                             </div>
-                            <hr />
-                        </div>
-                    );
-                }
-                return null;
-            })}
+                        );
+                    }
+                    return null;
+                })}
+            </div>
+
             <div className="cart-items-down">
                 <div className="cart-items-total">
                     <h1>Cart Totals</h1>
