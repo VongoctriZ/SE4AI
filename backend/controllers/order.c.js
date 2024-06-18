e1176f24b2787f39fd4764c53313f88b8dd31d33
huyinf <136902882+huyinf@users.noreply.github.com>
2024-06-19 02:42:01 +0700

    update status api

--- backend/controllers/order.c.js
+++ backend/controllers/order.c.js
@@ -1,8 +1,14 @@
+const axios = require('axios');
 const Order = require('../models/order.m');
 const User = require('../models/user.m');
 const Product = require('../models/product.m'); // Assuming you need this for validation
 
 class OrderController {
+
+    constructor() {
+        this.updateOrderStatus = this.updateOrderStatus.bind(this);
+    }
+
     // Create a new order
     async createOrder(req, res) {
         try {
@@ -45,6 +51,20 @@ class OrderController {
         }
     }
 
+    // Get all orders
+    async allOrders(req, res) {
+        try {
+            const orders = await Order.find({});
+            console.log("All orders fetched");
+            res.status(200).json(orders);
+        } catch (error) {
+            console.log("Error fetching all orders:", error);
+            res.status(500).json({
+                success: false,
+                message: 'Error fetching all orders',
+            });
+        }
+    }
     // Get all orders for a user
     async getUserOrders(req, res) {
         try {
@@ -83,26 +103,97 @@ class OrderController {
         }
     }
 
-    // Update the status of an order
-    async updateOrderStatus(req, res) {
+    async updateAttribute(req, res, attribute) {
         try {
-            const { orderId } = req.params;
-            const { status } = req.body;
+            console.log("request body: ", req.body);
+            const orderId = req.body.id;
+            const newValue = req.body[attribute];
 
-            // Find the order
-            const order = await Order.findOne({ id: orderId });
-            if (!order) {
-                return res.status(404).send({ message: 'Order not found' });
+            if (!newValue) {
+                return res.status(400).json({
+                    success: false,
+                    message: `New value for ${attribute} is required`,
+                });
+            };
+
+            // Fetch the current order to check its status
+            const currentOrder = await Order.findOne({ id: orderId });
+            if (!currentOrder) {
+                return res.status(404).json({
+                    success: false,
+                    message: 'Order not found',
+                });
             }
 
-            // Update the status
-            order.status = status;
-            await order.save();
+            // Check if the status is already 'accepted'
+            if (attribute === 'status' && currentOrder.status === 'accepted') {
+                return res.status(400).json({
+                    success: false,
+                    message: 'Order status is already accepted, no update performed',
+                });
+            }
 
-            res.status(200).send({ message: 'Order status updated successfully', order });
+            const updateData = {};
+            updateData[attribute] = newValue;
+
+            const updateOrder = await Order.findOneAndUpdate(
+                { id: orderId },
+                updateData,
+                { new: true, runValidators: true }
+            );
+
+            if (!updateOrder) {
+                return res.status(404).json({
+                    success: false,
+                    message: 'Order not found',
+                });
+            };
+
+            res.status(200).json({
+                success: true,
+                order: updateOrder,
+            });
+
+            // Additional step: If updating status to 'accepted', update products' all_time_quantity_sold
+            if (attribute === 'status' && newValue === 'accepted') {
+                await this.updateProductsQuantitySold(updateOrder.products);
+            }
+
         } catch (error) {
-            console.error('Error updating order status:', error);
-            res.status(500).send({ message: 'Error updating order status' });
+            console.error(`Error updating ${attribute}:`, error);
+            res.status(500).json({
+                success: false,
+                message: `Error updating ${attribute}`,
+            });
+        }
+    }
+
+    // Update the status of an order
+    async updateOrderStatus(req, res) {
+        return this.updateAttribute(req, res, 'status');
+    }
+
+    // Update products' all_time_quantity_sold based on order products
+    async updateProductsQuantitySold(products) {
+        try {
+            for (const product of products) {
+                const { productId, quantity } = product;
+                // Fetch the current product details
+                const existingProduct = await Product.findOne({ id: productId });
+                if (existingProduct) {
+                    console.log("Old quantity: ", existingProduct.all_time_quantity_sold);
+                    console.log("Add: ", quantity);
+
+                    const newQuantity = existingProduct.all_time_quantity_sold + quantity;
+                    await axios.post('http://localhost:4000/product/update/all_time_quantity_sold', {
+                        id: productId,
+                        all_time_quantity_sold: newQuantity
+                    });
+                }
+            }
+
+        } catch (error) {
+            console.error('Error updating products quantity sold:', error);
         }
     }
 
@@ -121,6 +212,25 @@ class OrderController {
         } catch (error) {
             console.error('Error deleting order:', error);
             res.status(500).send({ message: 'Error deleting order' });
+        }
+    }
+
+    // Remove all orders
+    async removeAllOrders(req, res) {
+        console.log("Request body: ", req.body);
+        try {
+            const result = await Order.deleteMany({});
+            console.log(`${result.deletedCount} orders removed`);
+            res.status(200).json({
+                success: true,
+                message: `${result.deletedCount} orders removed`,
+            });
+        } catch (error) {
+            console.log("Error removing all orders", error);
+            res.status(500).json({
+                success: 'false',
+                message: 'Error removing all orders',
+            });
         }
     }
 }
--- backend/routes/order.r.js
+++ backend/routes/order.r.js
@@ -5,6 +5,9 @@ const OrderController = require('../controllers/order.c');
 // Create a new order
 router.post('/create', OrderController.createOrder);
 
+// Get all orders
+router.get('/allorders',OrderController.allOrders);
+
 // Get all orders for a user
 router.get('/user/:userId', OrderController.getUserOrders);
 
@@ -12,9 +15,11 @@ router.get('/user/:userId', OrderController.getUserOrders);
 router.get('/:orderId', OrderController.getOrderDetails);
 
 // Update the status of an order
-router.put('/:orderId/status', OrderController.updateOrderStatus);
+router.post('/update/status', OrderController.updateOrderStatus);
 
 // Delete an order
 router.delete('/:orderId', OrderController.deleteOrder);
+
+router.post('/removeall', OrderController.removeAllOrders);
 
 module.exports = router;
